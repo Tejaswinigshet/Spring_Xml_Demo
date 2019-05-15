303e18c4d0e3cb7816d4b76e7164edc44a4dc9c5
Tejaswinigshet <tejaswinishet2@gmail.com>
2019-05-15 13:03:32 +0530

    spring task 6

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -1,17 +1,14 @@
 package com.stackroute.spring;
 import com.stackroute.spring.beans.Movie;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        applicationContext.registerShutdownHook();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie = (Movie)applicationContext.getBean("movie");
         System.out.println(movie.getActor());
 
     }
 }
-
 
