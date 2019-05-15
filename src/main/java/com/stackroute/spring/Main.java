97514685153799f68b3843de76e3e297973a3bb8
Tejaswinigshet <tejaswinishet2@gmail.com>
2019-05-15 11:15:11 +0530

    spring task 2

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -3,14 +3,18 @@ import com.stackroute.spring.beans.Actor;
 import com.stackroute.spring.beans.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie) context.getBean("movie");
-        System.out.println(movie.welcomeMsg()+"\nMovie name : "+movie.getMovieName()+"\nActor Name : "+movie.getActorNAme()+"\nGenre : "+movie.getGenre());
-        Actor actor = (Actor) context.getBean("actor");
-        System.out.println(actor.Msg()+"\nActorName : "+actor.getName()+"\ngender : "+actor.getGender()+"\nAge : "+actor.getAge());
 
+        //Using ApplicationContext
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie beanA = (Movie) context.getBean("MovieA");
+        Movie beanB = (Movie) context.getBean("MovieB");
+        Movie beanC = (Movie) context.getBean("MovieC");
+        System.out.println(beanA.toString());
+        System.out.println(beanB.toString());
+        System.out.println(beanC.toString());
+
+        System.out.println(beanA==beanB);
     }
 }
--- src/main/java/com/stackroute/spring/beans/Actor.java
+++ src/main/java/com/stackroute/spring/beans/Actor.java
@@ -1,31 +1,44 @@
 package com.stackroute.spring.beans;
+
 public class Actor {
     private String name;
     private String gender;
     private int age;
-    public Actor() {
+
+    Actor() {
+
     }
+
+    Actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getGender() {
         return gender;
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
-    public String Msg() {
-        return "Details about the Actor !";
-    }
+
     @Override
     public String toString() {
         return "Actor{" +
@@ -34,5 +47,5 @@ public class Actor {
                 ", age=" + age +
                 '}';
     }
-}
 
+}
