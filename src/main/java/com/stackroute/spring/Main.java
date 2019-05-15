d9a5a0b5aae2588f3c4a6eb9131d652e8fa462ab
Tejaswinigshet <tejaswinishet2@gmail.com>
2019-05-15 12:45:11 +0530

    spring task 5

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -1,14 +1,17 @@
 package com.stackroute.spring;
 import com.stackroute.spring.beans.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        applicationContext.registerShutdownHook();
         Movie movie = (Movie)applicationContext.getBean("movie");
         System.out.println(movie.getActor());
 
     }
 }
+
 
---
+++ src/main/java/com/stackroute/spring/beans/BeanLifeCycleDemo.java
@@ -0,0 +1,19 @@
+package com.stackroute.spring.beans;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
+    public void destroy() throws Exception {
+        System.out.println("Bean has been destroyed");
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean has been initialised");
+    }
+    public void customInit(){
+        System.out.println("This is custom init method");
+    }
+    public void customDestroy(){
+        System.out.println("This is custom destroy method");
+    }
+}
