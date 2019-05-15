b0ba47bea2942fb7eedbcc3f3aa44fb8fc202dfe
Tejaswinigshet <tejaswinishet2@gmail.com>
2019-05-15 13:59:09 +0530

    spring task 5

--- src/main/java/com/stackroute/spring/beans/Movie.java
+++ src/main/java/com/stackroute/spring/beans/Movie.java
@@ -1,6 +1,12 @@
 package com.stackroute.spring.beans;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+
 public class Movie {
     Actor actor = new Actor();
+    private BeanFactory beanFactory;
+    private Object applicationContextAware;
 
     public Actor getActor() {
         return actor;
@@ -8,6 +14,18 @@ public class Movie {
 
     public void setActor(Actor actor) {
         this.actor = actor;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException{
+        this.beanFactory=beanFactory;
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("Bean name is "+s);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContextAware=applicationContextAware;
     }
 
 }
